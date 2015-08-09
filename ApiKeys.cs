8ccab035d807f13dddbf49ed023fa37fca41b23c
David White <david.white323@gmail.com>
2015-08-08 22:48:21 -0600

    Adding a class that will hold api keys. Actual keys should not be committed.

---
+++ ApiKeys.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedditWallpapers
+{
+    class ApiKeys
+    {
+        public static string imgurClientId = "client_id here";
+        public static string imgurClientSecret = "client_secret_here";
+    }
+}
