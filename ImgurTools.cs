81207715772fb7bf828a91cb6e82f20852df7066
David White <david.white323@gmail.com>
2015-08-11 22:58:25 -0600

    Move Imgur tools to its own file. Add a method to use the imgur api to find the image type. Add a method to get the image id string from a full imgur url.
    The getImageType method requires adding the System.Web.Helpers extension as a reference to the project. Additionally it threw exceptions unless I unchecked the "Enable the Visual Studio hosting process" box in the debug section of the project properties.

---
+++ ImgurTools.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RedditWallpapers
+{
+    public class ImgurTools
+    {
+        string imgurAPIBaseURL = "https://api.imgur.com/3/";
+        WebClient webClient;
+        string tempPath;
+        string picsPath;
+        public ImgurTools()
+        {
+            this.webClient = new WebClient();
+            string sysTempPath = Path.GetTempPath();
+            tempPath = Path.Combine(sysTempPath, "redditWallpaperIcons");
+            picsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+        }
+
+        public ImageList getThumbnails(ArrayList postURLs)
+        {
+            if (!Directory.Exists(tempPath))
+            {
+                Directory.CreateDirectory(tempPath);
+            }
+            ImageList imageList = new ImageList();
+            imageList.ImageSize = new Size(90, 90);
+            imageList.ColorDepth = ColorDepth.Depth24Bit;
+            foreach (Uri uri in postURLs)
+            {
+                string ext = Path.GetExtension(uri.LocalPath);
+                string address = uri.AbsoluteUri.Substring(0, uri.AbsoluteUri.LastIndexOf(".")) + "s" + ext;
+                if (ext == "")
+                {
+                    // some URLs don't point directly to the image, so appending a dummy extension
+                    ext = ".png";
+                    address = uri.AbsoluteUri + "s" + ext;
+                }
+
+                string filename = Path.Combine(tempPath, Path.GetFileNameWithoutExtension(uri.LocalPath) + "_thumb" + ext);
+                webClient.DownloadFile(address, filename);
+                imageList.Images.Add(Image.FromFile(filename));
+            }
+            return imageList;
+        }
+
+        ~ImgurTools()
+        {
+            try
+            {
+                Directory.Delete(tempPath, true);
+            }
+            catch { } //don't care
+        }
+
+        string getImageType(string imageId)
+        {
+            string uri = imgurAPIBaseURL + "/image/" + imageId;
+            webClient.Headers.Add("Authorization", "Client-ID " + ApiKeys.imgurClientId);
+            string retval = webClient.DownloadString(uri);
+            dynamic json = System.Web.Helpers.Json.Decode(retval);
+            return json.data.type;
+        }
+
+        string getImageIdFromUrl(string url)
+        {
+            string imageid = url.Split('/').Last();
+            if (imageid.Contains('.'))
+            {
+                imageid = imageid.Substring(imageid.IndexOf('.'));
+            }
+            return imageid;
+        }
+
+        internal string getFullImage(string url, string localFilename)
+        {
+            string fullOutPath = Path.Combine(picsPath, localFilename);
+            //TODO: Use this imagetype here to decide on appropriate paths
+            string imagetype = getImageType(getImageIdFromUrl(url));
+            if (!url.StartsWith("http://i."))
+            {
+                url = url.Replace("http://", "http://i.");
+                if (!url.EndsWith(".png") || !url.EndsWith(".jpg"))
+                {
+                    //TODO: Implement Imgur API for pulling these images correctly
+                    url = url + ".png";
+                    fullOutPath += ".png";
+                }
+            }
+            webClient.DownloadFile(url, fullOutPath);
+            return fullOutPath;
+        }
+    }
+}
